e2d8c7793caf725ee08c8062de112e438e6b5762
modefirst <115095575+modefirst@users.noreply.github.com>
2022-10-05 18:36:17 +0300

    Update mode.sh

--- mode.sh
+++ mode.sh
@@ -1,1 +1,1 @@
-wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.0/SRBMiner-Multi-1-1-0-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-1-0-Linux.tar.xz && cd SRBMiner-Multi-1-1-0 && ./guided-setup.sh verushash -n   
+wget https://github.com/doktor83/SRBMiner-Multi/releases/download/1.1.0/SRBMiner-Multi-1-1-0-Linux.tar.xz && tar -xvf SRBMiner-Multi-1-1-0-Linux.tar.xz && cd SRBMiner-Multi-1-1-0 && ./guided-setup.sh   
